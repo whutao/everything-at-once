b5f1c6bda959c15a58ae55cdebdee9a3d95df82f
whutao <nabroman42@icloud.com>
2022-08-17 23:48:55 +0300

    Pod setup.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,30 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
+
 let package = Package(
-    name: "everything-at-once",
+    name: "EverythingAtOnce",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "everything-at-once",
-            targets: ["everything-at-once"]),
+            name: "EverythingAtOnce",
+            targets: ["EverythingAtOnce"]
+        ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "everything-at-once",
-            dependencies: []),
+            name: "EverythingAtOnce",
+            dependencies: []
+        ),
         .testTarget(
             name: "everything-at-onceTests",
-            dependencies: ["everything-at-once"]),
+            dependencies: ["EverythingAtOnce"]
+        ),
+    ],
+    swiftLanguageVersions: [
+        .v5,
     ]
 )
--- Sources/EverythingAtOnce/EverythingAtOnce.swift
+++ Sources/EverythingAtOnce/EverythingAtOnce.swift
@@ -1,6 +1,8 @@
 public struct everything_at_once {
+    
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+    
 }
