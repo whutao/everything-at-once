0dd785aa1e40b8a73429a56889e247dc5c2377ee
whutao <nabroman42@icloud.com>
2022-08-23 19:32:44 +0300

    Add attributed string extensions.8

--- Sources/EverythingAtOnce/Foundation/NSAttributedString+Attributes.swift
+++ Sources/EverythingAtOnce/Foundation/NSAttributedString+Attributes.swift
@@ -45,3 +45,113 @@ extension NSAttributedString {
     
 }
 #endif
+
+
+// MARK: Add attribute
+
+#if canImport(Foundation)
+import Foundation
+
+extension NSAttributedString {
+    
+    
+    /// Returns a new attributed string with applied attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - name: Key name.
+    ///   - value: Attribute value.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func addAttribute(_ name: NSAttributedString.Key, value: Any?, at range: NSRange? = nil) -> NSMutableAttributedString {
+        
+        let mutableAttributedString = NSMutableAttributedString(attributedString: self)
+        
+        guard let value = value else {
+            return mutableAttributedString
+        }
+
+        mutableAttributedString.addAttribute(name, value: value, range: range ?? fullRange)
+        return mutableAttributedString
+        
+    }
+    
+}
+#endif
+
+
+// MARK: Add attributes
+
+#if canImport(UIKit)
+import UIKit
+
+extension NSAttributedString {
+    
+    
+    /// Returns a new attributed string with modified text font attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - uiFont: Text font.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func font(_ uiFont: UIFont, at range: NSRange? = nil) -> NSMutableAttributedString {
+        return addAttribute(.font, value: uiFont, at: range)
+    }
+    
+    /// Returns a new attributed string with modified text color attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - uiColor: Text color.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func textColor(_ uiColor: UIColor?, at range: NSRange? = nil) -> NSMutableAttributedString {
+        return addAttribute(.foregroundColor, value: uiColor, at: range)
+    }
+    
+    /// Returns a new attributed string with modified background color attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - uiColor: Background color.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func backgroundColor(_ uiColor: UIColor?, at range: NSRange? = nil) -> NSMutableAttributedString {
+        return addAttribute(.backgroundColor, value: uiColor, at: range)
+    }
+    
+    /// Returns a new attributed string with modified paragraph attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - style: Paragraph style.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func paragraph(_ style: NSParagraphStyle?, at range: NSRange? = nil) -> NSMutableAttributedString {
+        return addAttribute(.paragraphStyle, value: style, at: range)
+    }
+    
+    /// Returns a new attributed string with modified shadow attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - nsShadow: Text shadow.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func shadow(_ nsShadow: NSShadow?, at range: NSRange? = nil) -> NSMutableAttributedString {
+        return addAttribute(.shadow, value: nsShadow, at: range)
+    }
+    
+    /// Returns a new attributed string with modified shadow attribute at the specified range.
+    ///
+    /// - Parameters:
+    ///   - color: Shadow color. Can be set `nil`.
+    ///   - offset: Shadow offset.
+    ///   - radius: Shadow radius.
+    ///   - range: Text range to apply an attribute. Consideres the whole string if `nil` is provided.
+    /// - Returns: A copy of the source string with applied attribute.
+    public func shadow(color: UIColor?, offset: CGSize = .zero, radius: CGFloat = .zero, at range: NSRange? = nil) -> NSMutableAttributedString {
+        let nsShadow = NSShadow()
+        nsShadow.shadowColor = color
+        nsShadow.shadowOffset = offset
+        nsShadow.shadowBlurRadius = radius
+        return shadow(nsShadow, at: range)
+    }
+
+}
+#endif
