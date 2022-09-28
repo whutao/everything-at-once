5940c2e5b26c1773504d40a1ee137863d87047c6
whutao <whutao.dev@gmail.com>
2022-09-28 19:57:04 +0300

    Add fastlane files.

---
+++ Sources/EverythingAtOnce/SwiftUI/PreviewDevice+Devices.swift
@@ -0,0 +1,84 @@
+//
+//
+//  MIT License
+//
+//  Copyright (c) 2022-Present EverythingAtOnce
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+
+public extension PreviewDevice {
+    
+    
+    static let iPhone6: PreviewDevice = .init(rawValue: "iPhone 6")
+    
+    static let iPhone6s: PreviewDevice = .init(rawValue: "iPhone 6s")
+    
+    static let iPhone6Plus: PreviewDevice = .init(rawValue: "iPhone 6 Plus")
+    
+    static let iPhone6sPlus: PreviewDevice = .init(rawValue: "iPhone 6s Plus")
+    
+    static let iPhoneSE1stGeneration: PreviewDevice = .init(rawValue: "iPhone SE (1st generation)")
+    
+    static let iPhoneSE2ndGeneration: PreviewDevice = .init(rawValue: "iPhone SE (2nd generation)")
+    
+    static let iPhone7: PreviewDevice = .init(rawValue: "iPhone 7")
+    
+    static let iPhone7Plus: PreviewDevice = .init(rawValue: "iPhone 7 Plus")
+    
+    static let iPhone8: PreviewDevice = .init(rawValue: "iPhone 8")
+    
+    static let iPhone8Plus: PreviewDevice = .init(rawValue: "iPhone 8 Plus")
+    
+    static let iPhoneX: PreviewDevice = .init(rawValue: "iPhone X")
+    
+    static let iPhoneXs: PreviewDevice = .init(rawValue: "iPhone Xs")
+    
+    static let iPhoneXsMax: PreviewDevice = .init(rawValue: "iPhone Xs Max")
+    
+    static let iPhoneXr: PreviewDevice = .init(rawValue: "iPhone Xʀ")
+    
+    static let iPhone11: PreviewDevice = .init(rawValue: "iPhone 11")
+    
+    static let iPhone11Pro: PreviewDevice = .init(rawValue: "iPhone 11 Pro")
+    
+    static let iPhone11ProMax: PreviewDevice = .init(rawValue: "iPhone 11 Pro Max")
+    
+    static let iPhone12mini: PreviewDevice = .init(rawValue: "iPhone 12 mini")
+    
+    static let iPhone12: PreviewDevice = .init(rawValue: "iPhone 12")
+    
+    static let iPhone12Pro: PreviewDevice = .init(rawValue: "iPhone 12 Pro")
+    
+    static let iPhone12ProMax: PreviewDevice = .init(rawValue: "iPhone 12 Pro Max")
+    
+    static let iPhone13: PreviewDevice = .init(rawValue: "iPhone 13")
+    
+    static let iPhone13Pro: PreviewDevice = .init(rawValue: "iPhone 13 Pro")
+    
+    static let iPhone13ProMax: PreviewDevice = .init(rawValue: "iPhone 13 Pro Max")
+    
+    static let iPhone13mini: PreviewDevice = .init(rawValue: "iPhone 13 mini")
+    
+}
+#endif
